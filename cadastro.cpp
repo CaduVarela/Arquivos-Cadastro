93e735984ca79196fb70a32db30c62b5c48276f8
Carlos Varela <caduvarela@gmail.com>
2022-11-21 14:03:06 -0300

    Adicionando Menu
    A partir daqui começam as implementações do sistema

--- cadastro.cpp
+++ cadastro.cpp
@@ -1,16 +1,64 @@
 #include <stdio.h>
 #include <string.h>
+#include <conio.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <windows.h>
 
+/*
+Funcionalidades
+
+( ) Inclusão
+( ) Exclusão
+( ) Alteração
+( ) Consulta
+( ) Listagem
+( ) Lixeira
+
+*/
+
+// Registro Funcionário
 typedef struct {
 	char CPF[12];
 	char nome[50];
 	float salario;
-	int situacao_cadastro;
+	int situacao_cadastro; // 0 --> ativo / 1 --> excluido
 } Funcionario;
 
 int main() {
-	Funcionario joao;
-	strcpy(joao.nome, "Joao Banana");
-	printf("%s", joao.nome);
+	int opcao, exit=0;
+	
+	setlocale(LC_ALL, "Portuguese");
+	
+	// Menu
+	do {
+		system("cls");
+		printf("-= MENU =-\n");
+		printf("1 - Adicionar\n");
+		printf("2 - Excluir\n");
+		printf("3 - Alterar\n");
+		printf("4 - Consultar\n");
+		printf("5 - Listar\n");
+		printf("6 - Lixeira\n");
+		printf("0 - Sair\n\n");
+		
+		printf("Selecione a opção: ");
+		opcao = getche(); /* getche() retorna o codigo ASCII da tecla */
+		opcao -= 48; /* tranforma o codigo da tecla no numero correspondente */
+		
+		printf("\n");
+		switch (opcao) {
+			case 0: // Sair / Fechar
+				printf("Fechando...\n");
+				Sleep(500);
+				exit = 1;
+				break;
+			default:
+				printf("Opção inválida!\n");
+				Sleep(500);
+				break;
+		}
+	} while (!exit);
+	
 	return 0;
 }
